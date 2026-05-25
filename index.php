9ce3f66869f81e0b58c5ea604268f2b88598f37b
nannphyo850-creator <nannphyo850@gmail.com>
2026-05-25 19:17:51 +0630

    first upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+echo "Hello World";
+echo "<h1>Super Friday</h1>";
+$language_type = "Sever-side programming language";
+echo $language_type;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<h1>This is paragraph</h1>";
+    ?>
+     <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim labore voluptatibus dignissimos natus voluptas quia suscipit. Itaque libero dignissimos, ducimus consectetur, natus, harum quisquam porro blanditiis ipsum eum totam impedit.
+     </p>
+     <script>
+        document.write('<h1>I am JavaScript!</h1>');
+    </script>
+</body>
+</html>
